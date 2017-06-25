93d9418a99b5338178a62fbe0be409c3ac2fe35f
mx-in <mouxin1993@gmail.com>
2017-06-25 13:14:55 +0800

    Init a Xcode Project

---
+++ YJRead/YJRead/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YJRead
+//
+//  Created by mx_in on 2017/6/25.
+//  Copyright © 2017年 mx_in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
