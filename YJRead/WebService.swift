2e5a70a7eef83b94b0e58b5a8ecef64ca7f310a3
mx_in <mouxin1993@gmail.com>
2019-05-07 16:29:43 +0800

    Create web service class

---
+++ YJRead/WebService.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  YJRead
+//
+//  Created by mx_in on 2019/4/30.
+//  Copyright © 2019 mx_in. All rights reserved.
+//
+
+import Foundation
+
+struct Resource<A> {
+    let url: URL
+    let parse: (Data) -> A?
+}
+
+extension Resource where A: Codable{
+    init(url: URL) {
+        self.url = url
+        self.parse = { data in
+            let model = try? JSONDecoder().decode(A.self, from: data)
+            return model
+        }
+    }
+}
+
+final class WebService {
+    
+    func load<A>(_ resource: Resource<A>, completion: @escaping (A?) -> ()) {
+        URLSession.shared.dataTask(with: resource.url) { data, _, _ in
+            let result = data.flatMap(resource.parse)
+            completion(result)
+        }.resume()
+    }
+    
+}
