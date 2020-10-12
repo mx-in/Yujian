f0ca3aa559741126639ed28d7521f3647ee691b1
mx_in <mouxin1993@gmail.com>
2020-10-12 23:33:31 +0800

    chore!: rename assets resources

---
+++ YJRead/View/YJSlideDownMenu.swift
@@ -0,0 +1,21 @@
+//
+//  YJSlideDownMenu.swift
+//  YJRead
+//
+//  Created by mx_in on 2020/10/12.
+//  Copyright © 2020 mx_in. All rights reserved.
+//
+
+import UIKit
+
+class YJSlideDownMenu: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
